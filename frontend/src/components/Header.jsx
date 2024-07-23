8e8ecb6380489b3fcdd978b369224e6d0bc1738c
Kunal Jain <kunaljain81384@gmail.com>
2024-07-23 15:51:43 +0530

    nav update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,41 +1,44 @@
 // src/components/Navbar.js
 import React from 'react';
-import logo from '../assets/lunarEdge_logo.png'; // Update the path to your logo
+import logo from '../assets/Lunaredge.png'; // Update the path to your logo
 import '../App.css'
 
 const Header = () => {
   return (
-    <nav className="hover:bg-blue-800 shadow-md fixed w-full top-10 left-0 z-50 h-16 bg-transparent transition-all duration-300 ">
+    <nav className="shadow-md fixed w-full top-10 left-0 z-50 h-20 bg-white transition-all duration-300  ">
       <div className="container mx-auto flex justify-around items-center h-full">
         {/* Logo */}
-        <div className="flex items-center">
-          <img src={logo} alt="Company Logo" className="h-[112px] w-[96px]" />
+        <div className='mt-4'>
+          <img src={logo} alt="Company Logo" className="h-[136px] w-[117px]" />
         </div>
 
         {/* Nav Links */}
         <ul className="hidden md:flex space-x-4 items-center">
           <li className='relative group'>
-           <a  className="text-white text-xl cursor-pointer hover:underline underline-offset-4 pl-6">About</a>
+           <a  className=" text-xl cursor-pointer hover:underline underline-offset-4 pl-6 text-[#082847]">About Us</a>
            {/* lists of about  */}
-           <ul className='absolute right-0 left-0 mt-2 top-4 rounded-lg shadow-lg transition-opacity duration-500 invisible group-hover:visible group-hover:opacity-100 bg-white w-60 pt-10 pb-3'>
-             <li><a href="/digital-innovators" className='block px-4 py-2 hover:underline underline-offset-4'>Digital innovators</a></li>
-             <li><a href="/clients" className='block px-4 py-2 hover:underline underline-offset-4 '>Our clients</a></li>
-             <li><a href="/careers" className='block px-4 py-2 hover:underline underline-offset-4'>careers</a></li>
+           <ul className=' absolute right-0 left-0 mt-2 top-1 rounded-lg shadow-lg transition-opacity duration-500 invisible group-hover:visible group-hover:opacity-100 bg-white w-60 pt-10 pb-3'>
+             <li><a href="/digital-innovators" className='block px-4 py-2 hover:underline underline-offset-4 text-[#082847]'>Digital innovators</a></li>
+             <li><a href="/clients" className='block px-4 py-2 hover:underline underline-offset-4 text-[#082847]'>Our clients</a></li>
+             <li><a href="/careers" className='block px-4 py-2 hover:underline underline-offset-4 text-[#082847]'>careers</a></li>
            </ul>
           </li>
           {/* Services  */}
           <li className='relative group'>
-          <a  className="text-white text-xl cursor-pointer hover:underline underline-offset-4 pl-6">Services</a>
+          <a  className=" text-xl cursor-pointer hover:underline underline-offset-4 pl-6 text-[#082847]"> Our Services</a>
           {/* lists inside services  */}
           <div>
             
           </div>
           </li>
           <li>
-            <a href="/about" className="text-white  text-xl  hover:underline underline-offset-4 pl-8">About</a>
+            <a href="/about" className="  text-xl  hover:underline underline-offset-4 pl-8 text-[#082847]">Industries</a>
           </li>
           <li>
-            <a href="/contact" className="text-white text-xl  hover:underline underline-offset-4 pl-8">Contact Us</a>
+            <a href="/about" className="  text-xl  hover:underline underline-offset-4 pl-8 text-[#082847]">Our Processes</a>
+          </li>
+          <li>
+            <a href="/contact" className=" text-xl  hover:underline underline-offset-4 pl-8 text-[#082847]">Contact Us</a>
           </li>
           {/* <li className="relative group">
             <a href="#" className="text-white text-xl hover:text-gray-300 focus:outline-none">
