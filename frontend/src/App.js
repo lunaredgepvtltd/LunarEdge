76e3e6ed5e4b1436ea216fa56810d4c01b13be27
Priyanshu-20025025 <priyanshusharma135a@gmail.com>
2024-07-25 18:36:55 +0530

    hero & footer updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Footer from './pages/Footer';
-
+import { Footer } from './pages/Footer';
 import Header from './pages/Header'; 
 import Hero from './pages/Hero';
+import { Form } from './pages/Form';
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
       <Hero/>
 
       {/* footer */}
-      <Footer/>
-       
+   
+       <Footer/>
     </div>
   );
 }
