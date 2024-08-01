eef536c7d7f9981547524d93ec13ed5aace3c462
Priyanshu-20025025 <priyanshusharma135a@gmail.com>
2024-08-01 15:26:09 +0530

    ui updated

--- frontend/src/components/TechBox.jsx
+++ frontend/src/components/TechBox.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import '../App.css'; // Import the CSS file
 
@@ -14,22 +13,3 @@ const TechBox = ({ logo, name }) => {
 }
 
 export default TechBox;
-=======
-import React from 'react'
-
-const TechBox = ({name,logo}) => {
-  return (
-    <div className=" flex flex-col justify-center items-center rounded-lg bg-blue-400 border w-[150px] h-[150px] shadow-md
-     hover:bg-black  transition-all duration-300 cursor-pointer text-white hover:shadow-white"> 
-    <span >
-      {logo}
-    </span>
-    <span className="text-xl  text-center w-[150px]" >{name}</span>
-  </div>
-  )
-}
-
-export default TechBox
-
-// this is snippet of component rafce
->>>>>>> dbd575ccde16e185986ae1d076be58cfffe0331c
