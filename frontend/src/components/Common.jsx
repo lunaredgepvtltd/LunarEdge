e28f99aea69b02720f9744c6c224bb9b4d45182d
kunal Jain <kunaljain81384@gmail.com>
2024-08-12 20:04:46 +0530

    small change in common.js

--- frontend/src/components/Common.jsx
+++ frontend/src/components/Common.jsx
@@ -5,7 +5,7 @@ const Common = () => {
   return (
     <div className='w-full h-[100vh] flex items-center justify-center'>
       <div className='flex flex-col justify-center gap-8'>
-      <h1 className='text-3xl md:text-8xl'>Work Under Processing</h1>
+      <h1 className='text-3xl md:text-8xl'>Work in Progress!</h1>
       <Link to={'/'} className='p-2 bg-green-400 text-white rounded-lg hover:text-green-400 hover:bg-white border border-green-400 transition-all duration-300 w-32'>Go To Home</Link>
       </div>
     </div>
