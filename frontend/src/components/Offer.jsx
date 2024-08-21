04825690927a581f173ea506445c3c5593cbc662
kunal Jain <kunaljain81384@gmail.com>
2024-08-21 16:25:48 +0530

    final commit for today

--- frontend/src/components/Offer.jsx
+++ frontend/src/components/Offer.jsx
@@ -7,7 +7,7 @@ const Offer = ({key,service,content}) => {
     <div 
     data-aos={isSmallDevice ? undefined : "fade-up"} 
     data-aos-delay="200"
-    className='flex flex-col justify-between h-full bg-blue-100 p-4 shadow-xl hover:shadow-md hover:shadow-blue-600 rounded-xl cursor-pointer hover:bg-white transition-all duration-200 ease-in hover:scale-95' 
+    className='flex flex-col justify-between h-full bg-blue-100 p-4 lg:py-16 shadow-xl hover:shadow-md hover:shadow-blue-600 rounded-xl cursor-pointer hover:bg-white transition-all duration-200 ease-in hover:scale-95' 
     key={key}
   >
     <h2 className='text-center text-xl md:text-3xl font-semibold pt-5 w-full'>
