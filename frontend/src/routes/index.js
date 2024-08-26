c8f047b924ac1cf17c868d2c2862af397fc5f20d
Lunaredgepvtltd <info@lunaredge.in>
2024-08-26 11:12:35 +0530

    added again aboutus

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App.js";
 import Common from "../components/Common.jsx";
 import Hero from "../pages/Hero.jsx";
-import { AboutUs } from "../components/AboutUs.jsx";
+import { AboutUs } from "../pages/AboutUs.jsx"
 const router = createBrowserRouter([
   {
     path: "/",
