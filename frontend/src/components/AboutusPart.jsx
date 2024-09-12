3c3f2ec28027c805778fcf218089fa7bd5e057ba
Priyanshu-20025025 <priyanshusharma135a@gmail.com>
2024-09-12 18:05:14 +0530

    testing

--- frontend/src/components/AboutusPart.jsx
+++ frontend/src/components/AboutusPart.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const AboutusPart = ({title,headline,content1,content2,sideImage}) => {
   return (
     <>
-      <div className="2xl:h-[600px] lg:h-[500px] md:h-[400px] lg:flex items-center">
+      <div className="2xl:h-[600px]  lg:h-[500px] md:h-[400px] lg:flex items-center">
         <div className="md:h-[450px] h-[530px] w-full md:flex md:justify-around px-3 md:px-0">
           <div className="lg:w-[40%] md:w-[46%] flex flex-col md:gap-4 gap-2 lg:text-base md:text-sm text-xs">
             <span className="text-[#2b1999]   flex items-center  ">
