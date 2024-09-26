494bb15f86427d620580e824b90edd518534ca62
Priyanshu-20025025 <priyanshusharma135a@gmail.com>
2024-09-26 11:24:52 +0530

    all components are created for new theme

--- frontend/src/components/ContactPage.jsx
+++ frontend/src/components/ContactPage.jsx
@@ -1,78 +1,40 @@
-import React from 'react';
-import { FiPhoneCall } from "react-icons/fi";
+import React from "react";
+import callContactPage from "../assets/callContactPage.png";
+import mailContactPage from "../assets/mailContactPage.png";
+import locContactPage from "../assets/locContactPage.png";
+import styles from "./ContactPage.module.css"; // Import CSS module
 
+
+const ContactCard = ({ image, children }) => (
+  <div className="w-[25%] bg-gradient-to-r from-pink-400 to-yellow-300 rounded-lg flex flex-col items-center justify-evenly px-5 shadow-lg">
+    <img src={image} alt="" className="h-[90px] w-[90px]" />
+    <div className="text-gray-800">{children}</div>
+  </div>
+);
 
 function ContactUs() {
   return (
     <div className="bg-custom-gradient py-16">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-8">How we can help you?</h2>
-        <h3 className="text-2xl font-semibold mb-12">CONTACT US</h3>
-        <div className="flex flex-wrap justify-center w-[70%] mx-auto">
-          {/* Phone */}
-          <div className="w-full md:w-1/3 px-4 mb-8">
-            <div className="bg-gradient-to-r from-pink-400 to-yellow-300 rounded-lg py-8 px-16 shadow-lg">
-              <div className="text-4xl text-gray-800 rounded-full  mx-auto mb-4 h-16 w-16 flex items-center justify-center">
-              <FiPhoneCall />
-              </div>
-              <p className="text-gray-800">0141-4512611 (Office)</p>
-              <p className="text-gray-800">+91-9829011076 (Sandeep Katariya)</p>
-              <p className="text-gray-800">+91-7760701155 (Sanjeev Ola)</p>
-            </div>
-          </div>
-          {/* Location */}
-          <div className="w-full md:w-1/3 px-4 mb-8">
-            <div className="bg-gradient-to-r from-pink-400 to-yellow-300 rounded-lg p-8 shadow-lg">
-              <div className="text-4xl text-gray-800 rounded-full  mx-auto mb-4 h-16 w-16 flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-8 w-8"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-              </div>
-              <p className="text-gray-800">
-                203, Manglam, Signature Tower , Lal Kothi, Gandhi Nagar, Jaipur,
-                Rajasthan - 302015
-              </p>
-            </div>
-          </div>
-          {/* Email */}
-          <div className="w-full md:w-1/3 px-4 mb-8">
-            <div className="bg-gradient-to-r from-pink-400 to-yellow-300 rounded-lg p-8 shadow-lg">
-              <div className="text-4xl text-gray-800 rounded-full  mx-auto mb-4 h-16 w-16 flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-8 w-8"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-              </div>
-              <p className="text-gray-800">info@lunaredge.in</p>
-            </div>
-          </div>
+        <h2 className="text-3xl font-normal mb-8">How we can help you?</h2>
+        <h3 className="text-5xl font-normal mb-12">CONTACT US</h3>
+        <div className=" h-[300px] flex flex-wrap justify-center gap-8 mx-auto ">
+          <ContactCard image={callContactPage}>
+            <p className="mb-5 text-center">0141-4512611 (Office)</p>
+            <p className="text-center">+91-9829011076 (Sandeep Katariya)</p>
+            <p className="text-center">+91-7760701155 (Sanjeev Ola)</p>
+          </ContactCard>
+
+          <ContactCard image={locContactPage}>
+            <p className="text-center">
+              203, Manglam, Signature Tower, Lal Kothi, Gandhi Nagar, Jaipur,
+              Rajasthan - 302015
+            </p>
+          </ContactCard>
+
+          <ContactCard image={mailContactPage}>
+            <p className="text-center">info@lunaredge.in</p>
+          </ContactCard>
         </div>
       </div>
     </div>
