53e0ff80f622f117dd98d88dcafb82bfd1b7db71
Priyanshu-20025025 <priyanshusharma135a@gmail.com>
2024-09-27 16:52:54 +0530

    core values updated

--- frontend/src/components/NewLandingPage.jsx
+++ frontend/src/components/NewLandingPage.jsx
@@ -4,22 +4,23 @@ import textimage from "../assets/textimage.png";
 
 const NewLandingPage = () => {
   return (
-    <div className="bg-custom-gradient h-screen pt-[60px] flex flex-col justify-evenly">
-      <h1 className="text-center text-4xl font-normal">ABOUT US</h1>
-      <div className="h-[430px]  flex  justify-evenly items-center">
+    <div className="bg-custom-gradient h-screen  pt-[4%] md:pt-[5%]  flex flex-col md:justify-evenly justify-center">
+      <h1 className="md:block hidden text-center text-4xl font-normal">ABOUT US</h1>
+      <div className="h-auto  flex md:flex-row flex-col md:gap-0 gap-3 justify-evenly items-center">
         <img
           src={hawaMahal}
           alt="Picture of HawaMahal"
-          className="h-[90%] w-[20%]"
+          className=" lg:h-[90%] lg:w-[20%] md:h-[70%] md:w-[30%] h-[55%] w-[55%]"
         />
-        <div className="w-[30%] space-y-8 text-xl font-normal text-left">
-          <p className="text-left">At LunarEdge, we transform <span className="italic">“ideas into reality”.</span></p>
-          <p className="text-left">
+        <h1 className="block md:hidden text-center text-xl font-normal">ABOUT US</h1>
+        <div className="md:w-[33%] w-[80%] xl:space-y-8 md:space-y-4 space-y-1 text-xl font-normal ">
+          <p className="hidden md:block text-left xl:text-xl text-sm ">At LunarEdge, we transform <span className="italic">“ideas into reality”.</span></p>
+          <p className="text-left xl:text-xl text-sm ">
             Founded with a vision to drive smart innovation, our mission is to
             bridge the gap between technology and human potential, empowering
             businesses to reach new heights.
           </p>
-          <p className="text-left">
+          <p className="hidden md:block text-left xl:text-xl text-sm">
             Our tagline, <span className="italic">“Smart Innovation Meets Infinity”</span>, reflects our
             commitment to endless possibilities and our pursuit of excellence in
             every project we undertake. Whether it’s developing cutting-edge
@@ -30,10 +31,10 @@ const NewLandingPage = () => {
         <img
           src={textimage}
           alt="Picture of tagline"
-          className="h-[70%] w-[20%]"
+          className="hidden lg:block lg:h-[70%] lg:w-[24%] "
         />
       </div>
-      <span className="flex justify-center "><button className="text-white rounded-full py-4 px-6 text-lg bg-gradient-to-r from-[#ff5757] to-[#8c52ff]">Get in Touch</button></span>
+      <span className="flex justify-center "><button className="text-white rounded-full md:py-4 md:px-6 py-2 px-3 md:text-lg text-sm bg-gradient-to-r from-[#ff5757] to-[#8c52ff]">Get in Touch</button></span>
     </div>
   );
 };
