8f3751f7713cfc2343afea8d3ff6f117e15da99a
Priyanshu-20025025 <priyanshusharma135a@gmail.com>
2024-09-30 18:11:24 +0530

    getInTouch created And Landing page created

--- frontend/src/pages/Hero2.jsx
+++ frontend/src/pages/Hero2.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
 import NewLandingPage from '../components/NewLandingPage'
-import CoreValues from '../components/CoreValues'
+import CoreValues from '../components/CoreValueSection/CoreValues'
 import NewGetInTouch from '../components/NewGetInTouch'
 import TransformingBusinessSection from '../components/TransformingBusinessSection'
 
 import ServicesNewTheme from '../components/ServicesNewTheme'
+import FirstPage from '../components/FirstPage'
 
 const Hero2 = () => {
   return (
     <div>
-      
+      <FirstPage/>
       <NewLandingPage/>
       <CoreValues/>
       <ServicesNewTheme/>    
