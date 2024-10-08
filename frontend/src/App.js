26a97cc86a331a8c3479beddbf48c8ddb880af13
kunal Jain <kunaljain81384@gmail.com>
2024-10-08 15:02:47 +0530

    hawa mahal dark mode chanegs and scroll small updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,19 +74,19 @@ function App() {
 
   return (
     <>
-    {console.log(scrollY)}
+    {/* {console.log(scrollY)} */}
       {isHome && (
-        <div className="hidden lg:flex flex-col items-center gap-8 fixed right-10 top-[30%] z-50 h-auto w-20 p-4">
+        <div className="hidden dark:text-white lg:flex flex-col items-center gap-8 fixed right-10 top-[30%] z-50 h-auto w-20 p-4">
           {/* Home */}
           <div>
             <div
-              className={`h-1 rounded-sm w-8 mx-auto bg-black cursor-pointer 
+              className={`h-1 rounded-sm w-8 mx-auto bg-black dark:bg-white cursor-pointer 
                 ${clickedSection === "home" || (scrollY < 725 && clickedSection === "") ? "scale-x-150" : ""} 
                 origin-right transition-all duration-300`}
               onClick={() => scrollToSection("home", 0)}
             ></div>
             {clickedSection === "home" || (scrollY < 725 && clickedSection === "") ? (
-              <div  className="text-[12px] font-medium" data-aos="fade-left">Home</div>
+              <div  className="text-[14px] pr-3 pt-1 font-medium" data-aos="fade-left">Home</div>
             ) : (
               ""
             )}
@@ -95,13 +95,13 @@ function App() {
           {/* About Us */}
           <div>
             <div
-              className={`h-1 rounded-sm w-8 mx-auto bg-black cursor-pointer 
+              className={`h-1 rounded-sm w-8 mx-auto bg-black  dark:bg-white  cursor-pointer 
                 ${clickedSection === "aboutUs" || (scrollY >= 725 && scrollY < 1424 && clickedSection === "") ? "scale-x-150" : ""} 
                 origin-right transition-all duration-300`}
               onClick={() => scrollToSection("aboutUs", 726)}
             ></div>
             {clickedSection === "aboutUs" || (scrollY >= 725 && scrollY < 1424 && clickedSection === "") ? (
-              <div className="flex flex-col items-center text-[12px] font-medium my-1" data-aos="fade-left">
+              <div className="flex flex-col items-center text-[14px] pr-3 pt-1 font-medium my-1" data-aos="fade-left">
                 <p>About</p>
                 <p>LunarEdge</p>
               </div>
@@ -113,13 +113,13 @@ function App() {
           {/* Hawa Mahal */}
           <div>
             <div
-              className={`h-1 rounded-sm w-8 mx-auto bg-black cursor-pointer 
+              className={`h-1 rounded-sm w-8 mx-auto bg-black  dark:bg-white  cursor-pointer 
                 ${clickedSection === "hawaMahal" || (scrollY >= 1424 && scrollY < 2033 && clickedSection === "") ? "scale-x-150" : ""} 
                 origin-right transition-all duration-300`}
               onClick={() => scrollToSection("hawaMahal", 1424)}
             ></div>
             {clickedSection === "hawaMahal" || (scrollY >= 1424 && scrollY < 2033 && clickedSection === "") ? (
-              <div className="text-[12px] font-medium text-nowrap" data-aos="fade-left">Hawa Mahal</div>
+              <div className="text-[14px] pr-3 pt-1 font-medium text-nowrap" data-aos="fade-left">Hawa Mahal</div>
             ) : (
               ""
             )}
@@ -128,13 +128,13 @@ function App() {
           {/* Journey */}
           <div>
             <div
-              className={`h-1 rounded-sm w-8 mx-auto bg-black cursor-pointer 
+              className={`h-1 rounded-sm w-8 mx-auto bg-black  dark:bg-white  cursor-pointer 
                 ${clickedSection === "journey" || (scrollY >= 2033 && scrollY < 2742 && clickedSection === "") ? "scale-x-150" : ""} 
                 origin-right transition-all duration-300`}
               onClick={() => scrollToSection("journey", 2034)}
             ></div>
             {clickedSection === "journey" || (scrollY >= 2033 && scrollY < 2742 && clickedSection === "") ? (
-              <div className="text-[12px] font-medium text-nowrap" data-aos="fade-left">Our Journey</div>
+              <div className="text-[14px] pr-3 pt-1 font-medium text-nowrap" data-aos="fade-left">Our Journey</div>
             ) : (
               ""
             )}
@@ -143,13 +143,13 @@ function App() {
           {/* Values */}
           <div>
             <div
-              className={`h-1 rounded-sm w-8 mx-auto bg-black cursor-pointer 
+              className={`h-1 rounded-sm w-8 mx-auto bg-black  dark:bg-white  cursor-pointer 
                 ${clickedSection === "values" || (scrollY >= 2742 && scrollY <= 3380 && clickedSection === "") ? "scale-x-150" : ""} 
                 origin-right transition-all duration-300`}
               onClick={() => scrollToSection("values", 2743)}
             ></div>
             {clickedSection === "values" || (scrollY >= 2742 && scrollY < 3380 && clickedSection === "") ? (
-              <div className="text-[12px] font-medium" data-aos="fade-left">Values</div>
+              <div className="text-[14px] pr-3 pt-1 font-medium" data-aos="fade-left">Values</div>
             ) : (
               ""
             )}
@@ -158,13 +158,13 @@ function App() {
           {/* Vision & Mission */}
           <div>
             <div
-              className={`h-1 rounded-sm w-8 mx-auto bg-black cursor-pointer 
+              className={`h-1 rounded-sm w-8 mx-auto bg-black  dark:bg-white  cursor-pointer 
                 ${clickedSection === "visionMission" || (scrollY >= 3380 && scrollY < 4656 && clickedSection === "") ? "scale-x-150" : ""} 
                 origin-right transition-all duration-300`}
               onClick={() => scrollToSection("visionMission", 3380)}
             ></div>
             {clickedSection === "visionMission" || (scrollY >= 3380 && scrollY < 4656 && clickedSection === "") ? (
-               <div className="text-[12px] font-medium text-nowrap" data-aos="fade-left">Vission & Mission</div>
+               <div className="text-[14px] pr-3 pt-1 font-medium text-nowrap" data-aos="fade-left">Vission & Mission</div>
             ) : (
               ""
             )}
@@ -173,13 +173,13 @@ function App() {
           {/* Technology */}
           <div>
             <div
-              className={`h-1 rounded-sm w-8 mx-auto bg-black cursor-pointer 
+              className={`h-1 rounded-sm w-8 mx-auto bg-black  dark:bg-white  cursor-pointer 
                 ${clickedSection === "technology" || (scrollY >= 4656 && scrollY <= 5463 && clickedSection === "") ? "scale-x-150" : ""} 
                 origin-right transition-all duration-300`}
               onClick={() => scrollToSection("technology", 4656)}
             ></div>
             {clickedSection === "technology" || (scrollY >= 4656 && scrollY <= 5460 && clickedSection === "") ? (
-              <div className="text-[12px] font-medium" data-aos="fade-left">Technology</div>
+              <div className="text-[14px] pr-3 pt-1 font-medium" data-aos="fade-left">Technology</div>
             ) : (
               ""
             )}
@@ -188,13 +188,13 @@ function App() {
           {/* Contact */}
           <div>
             <div
-              className={`h-1 rounded-sm w-8 mx-auto bg-black cursor-pointer 
+              className={`h-1 rounded-sm w-8 mx-auto bg-black  dark:bg-white  cursor-pointer 
                 ${clickedSection === "contact" || (scrollY > 5460 && clickedSection === "") ? "scale-x-150" : ""} 
                 origin-right transition-all duration-300`}
               onClick={() => scrollToSection("contact", 5460)}
             ></div>
             {clickedSection === "contact" || (scrollY > 5460 && clickedSection === "") ? (
-              <div className="text-[12px] font-medium text-nowrap" data-aos="fade-left">Contact Us</div>
+              <div className="text-[14px] pr-3 pt-1 font-medium text-nowrap" data-aos="fade-left">Contact Us</div>
             ) : (
               ""
             )}
@@ -211,7 +211,7 @@ function App() {
         {/* Outlet for nested routes */}
         <Suspense fallback={<div>Loading Content...</div>}>
           <ScrollToTop />
-          <Outlet />
+          <Outlet context={{ darkMode }} />
         </Suspense>
       </main>
 
