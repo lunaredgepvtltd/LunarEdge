d2b11d6b4f3dbdfb8c1f77b3f0e490089013fdf5
Priyanshu-20025025 <priyanshusharma135a@gmail.com>
2024-10-09 16:26:56 +0530

    ask

--- frontend/src/components/NewGetInTouch/NewGetInTouch.jsx
+++ frontend/src/components/NewGetInTouch/NewGetInTouch.jsx
@@ -49,7 +49,7 @@ const GetInTouch = () => {
  }
 
   return (
-    <div className="flex md:flex-row flex-col-reverse items-center w-[100%] h-full bg-white dark:bg-gray-800  p-4">
+    <div className="flex md:flex-row flex-col-reverse items-center w-[100%] h-full bg-white dark:bg-black  md:pb-10 p-4">
       <div className="md:w-[60%] p-6 w-full">
         <div className="flex flex-col items-center w-full">
           <h2 className="xl:text-5xl md:text-3xl font-bold text-gray-800 dark:text-white mb-4">
