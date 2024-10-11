5ccf9736544448d1b425e3da6112fceb45e583d1
kunal Jain <kunaljain81384@gmail.com>
2024-10-11 15:07:19 +0530

    aboutUs reponsiveness updated

--- frontend/src/components/NewLandingPage/NewLandingPage.jsx
+++ frontend/src/components/NewLandingPage/NewLandingPage.jsx
@@ -12,17 +12,17 @@ const NewLandingPage = () => {
   return (
 <>
 <div id="aboutUs" className="h-[90px] w-full hidden lg:block"></div>
-<div  className="relative w-full  dark:text-white  md:h-screen h-[415px] flex">
-      <div className=" md:w-[44%] w-[100%] relative h-[375px]  md:h-full">
+<div  className="relative w-full  dark:text-white  lg:h-screen h-[415px] flex">
+      <div className=" md:w-[52%] xl:w-[44%] w-[100%] relative h-[375px]  md:h-full">
         <img
           src={`${isExtraSmallDevice ? smallAboutMoon : aboutMoon}`}
           alt="Picture of Moon"
-          className="absolute -left-[78%] md:-left-[66%] md:h-[95%] h-full w-full  animate-spin-slow"
+          className="absolute -left-[78%] md:-left-[66%] lg:-left-[64%] xl:-left-[66%] md:h-[95%] lg:h-[85%] xl:h-[95%] h-full w-full  animate-spin-slow"
         />
       </div>
 
-      <div className="z-10 absolute left-[23%] top-[9%] w-[74%] md:w-[75%] h-auto md:h-full md:flex items-start gap-4">
-        <div className="w-full md:w-[50%] h-full md:h-[63%] flex flex-col gap-5 xl:mt-6">
+      <div className="z-10 absolute left-[23%] md:left-[19%] lg:left-[23%] top-[9%] w-[74%] md:w-[75%] h-auto lg:h-full lg:flex items-start gap-4">
+        <div className="w-full lg:w-[50%] h-full md:h-[63%] flex flex-col gap-5 xl:mt-6">
           <h1 className=" text-left text-3xl font-medium ">ABOUT US</h1>
           <p className="text-left font-normal text-[14px] md:text-lg xl:text-2xl">
             Founded with a vision to drive smart innovation, our mission is to
