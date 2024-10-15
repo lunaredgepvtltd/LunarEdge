ccd0b77d209c323effaf9f88e659255cdfb2c572
sujeet3008 <ranjansujeet34@gmail.com>
2024-10-15 17:15:07 +0530

    how we do (sujeet)

--- frontend/src/components/HowWeDo/HowWeDo.jsx
+++ frontend/src/components/HowWeDo/HowWeDo.jsx
@@ -10,17 +10,17 @@ const HowWeDo = () => {
       <img
         src={bgHowWeDo}
         alt="How We Do Background"
-        className="absolute inset-0 z-0 mt-[10%] md:w-[90%] md:h-[90%] w-full h-full object-contain opacity-50 pointer-events-none"
+        className="absolute inset-0 z-0 mt-[10%] md:w-[90%] md:h-[90%] w-full h-full object-contain opacity-50 pointer-events-none  "
       />
 
-      <div className="h-full  z-10 flex flex-col items-center justify-center py-0 md:py-10 absolute inset-0">
-        <h1 className=" text-2xl md:text-5xl  font-normal mb-10 text-black dark:text-white">
+      <div className="h-full  z-10 flex flex-col justify-evenly items-center md:justify-center pt-[5%]  md:pt-[13%] pr-[4%]  md:pr-0 absolute inset-0">
+        <h1 className=" text-2xl md:text-5xl  font-normal md:mb-10 text-black dark:text-white">
           Process We follow
         </h1>
         <h4 className=" text-xl md:text-3xl font-normal  mb-0 md:mb-10 text-black dark:text-white">
           How we do
         </h4>
-        <div className="max-w-xs md:max-w-md m-auto ">
+        <div className="max-w-xs md:max-w-md mt-4 md:m-auto ">
           {/* Step 1 */}
           <div className="flex items-center ">
             <div className="w-14 h-14 md:w-28 md:h-28 bg-gradient-to-r from-[#5d5d5d] to-[#ffffff] dark:bg-gradient-to-r dark:from-[#5d5d5d] dark:to-[#ffffff] rounded-full"></div>
@@ -29,7 +29,7 @@ const HowWeDo = () => {
             </div>
           </div>
 
-          <div className="w-full flex flex-col justify-center ml-10 ">
+          <div className="w-full flex flex-col justify-center ml-7 md:ml-10 ">
             <div className="text-center border-2 border-black dark:border-white w-[90%] h-[2rem] border-dashed border-y-0 border-r-0 rounded-bl-lg"></div>
             <div className="text-center border-2 ml-2 border-black dark:border-white w-[80%] h-[3rem] border-l-0 border-b-0 border-dashed rounded-tr-lg "></div>
           </div>
@@ -40,7 +40,7 @@ const HowWeDo = () => {
             </div>
             <div className="w-14 h-14 md:w-28 md:h-28 bg-gradient-to-r from-[#5d5d5d] to-[#ffffff]  rounded-full"></div>
           </div>
-          <div className="w-full flex flex-col justify-center ml-10 ">
+          <div className="w-full flex flex-col justify-center ml-7 md:ml-10 ">
             <div className="text-center border-2 ml-2 border-black dark:border-white w-[80%] h-[2rem] border-l-0 border-t-0 border-dashed rounded-br-lg"></div>
             <div className="text-center border-2 border-black dark:border-white w-[90%] h-[2rem] border-dashed border-y-0 border-r-0 rounded-tl-lg"></div>
           </div>
@@ -52,7 +52,7 @@ const HowWeDo = () => {
               Design and <br /> Prototyping
             </div>
           </div>
-          <div className="w-full flex flex-col justify-center ml-10 ">
+          <div className="w-full flex flex-col justify-center ml-7 md:ml-10 ">
             <div className="text-center border-2 border-black  dark:border-white w-[90%] h-[2rem] border-dashed border-y-0 border-r-0 rounded-bl-lg"></div>
             <div className="text-center border-2 ml-2 border-black dark:border-white w-[80%] h-[3rem] border-l-0 border-b-0 border-dashed rounded-tr-lg "></div>
           </div>
@@ -63,19 +63,19 @@ const HowWeDo = () => {
             </div>
             <div className="w-14 h-14 md:w-28 md:h-28 bg-gradient-to-r from-[#5d5d5d] to-[#ffffff]  rounded-full"></div>
           </div>
-          <div className="w-full flex flex-col justify-center ml-10">
+          <div className="w-full flex flex-col justify-center ml-7 md:ml-10">
             <div className="text-center border-2 ml-2 border-black dark:border-white w-[80%] h-[2rem] border-l-0 border-t-0 border-dashed rounded-br-lg"></div>
             <div className="text-center border-2 border-black dark:border-white w-[90%] h-[2rem] border-dashed border-y-0 border-r-0 rounded-tl-lg"></div>
           </div>
           {/* Step 5 */}
           <div className="flex items-center ">
-            <div className="w-14 h-14 md:w-28 md:h-28  bg-gradient-to-r from-[#5d5d5d] to-[#ffffff]  rounded-full"></div>
-            <div className=" bg-gradient-to-r from-[#5d5d5d] to-[#dddbdb] dark:bg-gradient-to-r dark:from-[#000000] dark:to-[#737373] text-sm md:text-xl  rounded-full p-3 md:p-6 text-black dark:text-white text-center m-auto font-semibold">
-              Deployment  and  Implementation
+            <div className="w-14 h-14 md:w-28 md:h-28 bg-gradient-to-r from-[#5d5d5d] to-[#ffffff]  rounded-full"></div>
+            <div className="flex-1 bg-gradient-to-r from-[#5d5d5d] to-[#dddbdb] dark:bg-gradient-to-r dark:from-[#000000] dark:to-[#737373] text-sm md:text-xl  rounded-full p-3 md:p-6 text-black dark:text-white text-center m-auto font-semibold">
+              Deploy and <br /> Implementation 
             </div>
           </div>
           <div className="flex items-center justify-center">
-            <div className="w-full flex flex-col justify-center ml-10 ">
+            <div className="w-full flex flex-col justify-center ml-7 md:ml-10 ">
               <div className="text-center border-2 border-black dark:border-white w-[90%] h-[2rem] border-dashed border-y-0 border-r-0 rounded-bl-lg"></div>
               <div className="text-center border-2 ml-2 border-black dark:border-white w-[90%] h-[3rem] border-l-0 border-b-0 border-dashed rounded-tr-lg "></div>
             </div>
