0d94c01ed2809531073d409b88be4ea2d611b391
Priyanshu-20025025 <priyanshusharma135a@gmail.com>
2024-10-16 15:12:51 +0530

    middleCareer Responsive

--- frontend/src/components/MiddleCareer.jsx
+++ frontend/src/components/MiddleCareer.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 
 // Component for Job Opening
 const JobOpening = ({ title, experience, location, jobType, buttonText}) => {
-  Const[seto]
+
   return (
-    <div className="border-2 w-full border-black p-4 md:p-6 rounded-xl flex flex-col md:flex-row justify-between items-center">
-      <div className="mb-4 md:mb-0 text-center md:text-left">
-        {jobType && <h2 className="text-md md:text-lg text-[#2e2e30]">{jobType}</h2>}
-        <h2 className="text-xl md:text-2xl font-semibold text-[#160962]">{title}</h2>
-        <div className="flex flex-col md:flex-row md:space-x-4 mt-4 text-gray-600">
+    <div className="border-2 w-full border-black p-2 md:p-6 rounded-xl flex flex-row lg:justify-between items-start md:items-center">
+      <div className="mb-4 md:mb-0 w-[70%] lg:w-auto text-left">
+        {jobType && <h2 className="text-sm md:text-lg text-[#2e2e30]">{jobType}</h2>}
+        <h2 className="text-md md:text-2xl w-[80%] lg:w-[100%] font-semibold text-[#160962]">{title}</h2>
+        <div className="flex  text-xs md:text-sm flex-row space-x-2 md:space-x-4 mt-4 text-gray-600">
           <div className="flex items-center mb-2 md:mb-0">
-            <span className="mr-2">&#x1F4BC;</span> {experience}
+            <span className=" px-1 ">&#x1F4BC;</span> {experience}
           </div>
-          <div className="flex items-center">
-            <span className="mr-2">&#x1F4CD;</span> {location}
+          <div className="flex items-center  ">
+            <span className=" px-1">&#x1F4CD;</span> {location}
           </div>
         </div>
       </div>
-      <button className={`bg-gradient-to-r from-[#ff5757] to-[#8c52ff] text-white px-4 py-2 md:px-6 md:py-3 rounded-3xl font-semibold tracking-wider`} >
+      <button className={`w-[30%] md:w-auto text-xs md:text-sm bg-gradient-to-r from-[#ff5757] to-[#8c52ff] text-white px-2 py-2 md:px-6 md:py-3 rounded-3xl md:font-semibold md:tracking-wider`} >
         {buttonText}
       </button>
     </div>
@@ -46,7 +46,7 @@ const MiddleCareer = () => {
 
   return (
     <div className="bg-white min-h-screen w-full flex justify-center items-center">
-      <div className="w-[90%] md:w-[80%] px-4 py-8">
+      <div className="w-[95%] md:w-[80%] px-4 py-8">
         <h1 className="text-center text-3xl md:text-4xl font-semibold text-black mb-8">Openings</h1>
         
         <div className="space-y-6 w-full">
