8c6dd84c68c9b91a78365f2cba69e2e4b2bc8d88
sujeet3008 <ranjansujeet34@gmail.com>
2024-10-17 23:32:01 +0530

    black theme middle career

--- frontend/src/components/MiddleCareer.jsx
+++ frontend/src/components/MiddleCareer.jsx
@@ -23,14 +23,14 @@ const JobOpening = ({ jobTitle, experience, location, togglePopup, jobId , jobSu
   }
 
   return (
-    <div className="border-2 w-full border-black p-2 md:p-6 rounded-xl flex flex-row lg:justify-between items-start md:items-center">
+    <div className="border-2 w-full border-black dark:border-white p-2 md:p-6 rounded-xl flex flex-row lg:justify-between items-start md:items-center">
       <div className="mb-4 md:mb-0 w-[70%] lg:w-auto text-left">
-        <h2 className="text-sm md:text-lg text-[#2e2e30]">ON SITE</h2>
+        <h2 className="text-sm md:text-lg text-[#2e2e30] dark:text-white">ON SITE</h2>
         <h2
-          className="text-md md:text-2xl w-[80%] lg:w-[100%] font-semibold text-[#160962]"
+          className="text-md md:text-2xl w-[80%] lg:w-[100%] font-semibold text-[#160962] dark:text-pink-500"
           dangerouslySetInnerHTML={{ __html: jobTitle }}
         />
-        <div className="flex text-xs md:text-sm flex-row space-x-2 md:space-x-4 mt-4 text-gray-600">
+        <div className="flex text-xs md:text-sm flex-row space-x-2 md:space-x-4 mt-4 text-gray-600 dark:text-white">
           <div className="flex items-center mb-2 md:mb-0">
             <span className="px-1">&#x1F4BC;</span> <span dangerouslySetInnerHTML={{ __html: experience }} />
           </div>
@@ -117,10 +117,10 @@ const MiddleCareer = () => {
   }, []);
 
   return (
-    <div className="bg-white min-h-screen w-full flex flex-col justify-center items-center">
+    <div className="bg-white dark:bg-[#131313] min-h-screen w-full flex flex-col justify-center items-center">
       <div className="w-[95%] md:w-[80%] px-4 py-8">
         <div className="flex flex-col justify-center items-center py-2">
-          <h1 className="text-center text-3xl md:text-4xl font-semibold text-black mb-8">Openings</h1>
+          <h1 className="text-center text-3xl md:text-4xl font-semibold text-black mb-[8rem]">Openings</h1>
           {user?.role === 'ADMIN' ? (
             <button
               className="w-[30%] md:w-auto text-xs md:text-sm bg-gradient-to-r from-[#ff5757] to-[#8c52ff] text-white px-2 py-2 md:px-6 md:py-3 rounded-3xl md:font-semibold md:tracking-wider"
