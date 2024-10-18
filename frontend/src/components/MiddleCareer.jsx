b643e8cabf9089325ec62f363c41a92c9964aad3
Priyanshu-20025025 <priyanshusharma135a@gmail.com>
2024-10-18 13:53:52 +0530

    responsive middle career

--- frontend/src/components/MiddleCareer.jsx
+++ frontend/src/components/MiddleCareer.jsx
@@ -6,6 +6,10 @@ import { API } from '../helper/index.js';
 import { MdDeleteOutline } from "react-icons/md";
 import { FaRegEdit } from "react-icons/fa";
 import NewUpdateVacancy from './NewUpdateVacancy/NewUpdateVacancy.jsx';
+import { MdOutlineLocationOn } from "react-icons/md";
+import { FaBriefcase } from "react-icons/fa";
+
+
 
 // Component for Job Opening
 const JobOpening = ({ jobTitle, experience, location, togglePopup, jobId , jobSummary, keyResponsibilities, qualificationAndSkills, preferredQualifications, whatWeOffer , fetchVacancyDetails}) => {
@@ -30,18 +34,19 @@ const JobOpening = ({ jobTitle, experience, location, togglePopup, jobId , jobSu
           className="text-md md:text-2xl w-[80%] lg:w-[100%] font-semibold text-[#160962] dark:text-pink-500"
           dangerouslySetInnerHTML={{ __html: jobTitle }}
         />
-        <div className="flex text-xs md:text-sm flex-row space-x-2 md:space-x-4 mt-4 text-gray-600 dark:text-white">
+        <div className="flex text-xs md:text-sm md:flex-row flex-col  md:space-x-4 mt-4 text-gray-600 dark:text-white">
           <div className="flex items-center mb-2 md:mb-0">
-            <span className="px-1">&#x1F4BC;</span> <span dangerouslySetInnerHTML={{ __html: experience }} />
+            <span className="px-1"><FaBriefcase className='text-pink-500' />
+            </span> <span dangerouslySetInnerHTML={{ __html: experience }} />
           </div>
           <div className="flex items-center">
-            <span className="px-1">&#x1F4CD;</span> <span dangerouslySetInnerHTML={{ __html: location }} />
+            <span className='px-1'><MdOutlineLocationOn className='text-pink-500' /></span> <span dangerouslySetInnerHTML={{ __html: location }} />
           </div>
         </div>
       </div>
       <div className='flex flex-col gap-3 items-center justify-center'>
         <button
-          className="w-[30%] md:w-auto text-xs md:text-sm bg-gradient-to-r from-[#ff5757] to-[#8c52ff] text-white px-2 py-2 md:px-6 md:py-3 rounded-3xl md:font-semibold md:tracking-wider"
+          className="w-[100%] md:w-auto text-xs md:text-sm bg-gradient-to-r from-[#ff5757] to-[#8c52ff] text-white px-2 py-2 md:px-6 md:py-3 rounded-3xl md:font-semibold md:tracking-wider"
           onClick={() => togglePopup(jobId)}
         >
           Apply Now
@@ -49,7 +54,7 @@ const JobOpening = ({ jobTitle, experience, location, togglePopup, jobId , jobSu
         {user?.role === "ADMIN" && (
           <div className="flex items-center justify-between w-[60%]">
             {/* edit  */}
-            <FaRegEdit className="text-xl text-black cursor-pointer hover:text-red-500" onClick={() => {
+            <FaRegEdit className="text-xl text-black cursor-pointer dark:text-white hover:text-red-500" onClick={() => {
               setUpdateVacancy(jobId);
             }} />
 
@@ -71,7 +76,7 @@ const JobOpening = ({ jobTitle, experience, location, togglePopup, jobId , jobSu
             )}
 
             {/* delete  */}
-            <MdDeleteOutline className="text-2xl cursor-pointer hover:text-red-500" onClick={() => {
+            <MdDeleteOutline className="text-2xl cursor-pointer dark:text-white hover:text-red-500" onClick={() => {
               setDeleteVacancy(jobId);
             }} />
           </div>
@@ -120,10 +125,10 @@ const MiddleCareer = () => {
     <div className="bg-white dark:bg-[#131313] min-h-screen w-full flex flex-col justify-center items-center">
       <div className="w-[95%] md:w-[80%] px-4 py-8">
         <div className="flex flex-col justify-center items-center py-2">
-          <h1 className="text-center text-3xl md:text-4xl font-semibold text-black mb-[8rem]">Openings</h1>
+          <h1 className="text-center text-3xl md:text-4xl font-semibold dark:text-white text-black mb-[8rem]">Openings</h1>
           {user?.role === 'ADMIN' ? (
             <button
-              className="w-[30%] md:w-auto text-xs md:text-sm bg-gradient-to-r from-[#ff5757] to-[#8c52ff] text-white px-2 py-2 md:px-6 md:py-3 rounded-3xl md:font-semibold md:tracking-wider"
+              className="w-[40%] md:w-auto text-xs md:text-sm bg-gradient-to-r from-[#ff5757] to-[#8c52ff] text-white px-2 py-2 md:px-6 md:py-3 rounded-3xl md:font-semibold md:tracking-wider"
               onClick={() => setShowAddVacancy(true)}
             >
               Add New Vacancy
@@ -145,6 +150,23 @@ const MiddleCareer = () => {
 
       {/* Show the popup if isPopupOpen is true and pass the selectedJobId */}
       {isPopupOpen && <PopupCareer jobId={selectedJobId} togglePopup={togglePopup} />}
+
+
+      {/* career bottom section  */}
+
+      <div className="z-10 flex-col flex justify-center items-center  h-[250px] md:h-[450px] px-4">
+       
+       <p className="text-black dark:text-white text-md md:text-2xl max-w-5xl text-center mx-auto px-4">
+         Don’t see a position that matches your skills? Send us your resume at{" "}
+         <a
+           href="mailto:hr@lunaredge.com"
+           className="text-black dark:text-white underline"
+         >
+           hr@lunaredge.com
+         </a>
+         . We’d love to hear from you!
+       </p>
+     </div>
     </div>
   );
 };
--- frontend/src/pages/NewCareer.jsx
+++ frontend/src/pages/NewCareer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MiddleCareer from '../components/MiddleCareer'
 import CareerLanding from '../components/CareerOverview/CareerLanding.jsx';
-import Careerbottom from '../components/CareerOverview/Careerbottom.jsx';
+
 import PopupCareer from '../components/PopupCareer.jsx';
 
 const NewCareer = () => {
@@ -11,7 +11,7 @@ const NewCareer = () => {
       
     <MiddleCareer/>
     {/* <PopupCareer/> */}
-    <Careerbottom />
+ 
 
     </div>
   )
